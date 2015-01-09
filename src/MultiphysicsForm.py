5a145f6b4afee8deff8176927b3e1cee0df41763
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-09 15:25:14 -0500

    Added gauss loop.

--- src/MultiphysicsForm.py
+++ src/MultiphysicsForm.py
@@ -55,6 +55,52 @@ def MultiphysicsForm(W,V,S,wx,wv,X0):
     AVTot = None
     Mass = None
     ThermalMass = None
-    
+
     J0 = Constant(Width*Height/4.0 )
+    for z1,z2,weight in GPS:
+        u = r + Height/2.0*Constant(z1)*g1 + Width/2.0*Constant(z2)*g2
+        v = vr + Height/2.0*Constant(z1)*vg1 + Width/2.0*Constant(z2)*vg2
+        
+        # dxdt = drdt + Height/2.0*Constant(z1)*dg1dt + Width/2.0*Constant(z1)*dg2dt
+        dvdt = dvrdt + Height/2.0*Constant(z1)*dvg1dt + Width/2.0*Constant(z2)*dvg2dt
+        du = derivative(u,wx,dw)
+        dv = derivative(v,wv,dw)
+        dT = derivative(T,wv,dw)
+        dVol = derivative(Vol,wv,dw)
+        
+        I = Identity(V.cell().geometric_dimension())
+        F = I + outer(u.dx(0),Constant((1.0,0.0,0.0))) + outer(g1,Constant((0.0,1.0,0.0))) + outer(g2,Constant( (0.0,0.0,1.0) ))
+        C = F.T*F
+        Ic = tr(C)
+        J = det(F)
+        
+        MassCont = inner(dv,rho*dvdt)
+        PsiCont = Constant(weight)*((mu/2)*(Ic - 3) - mu*ln(J) + (lmbda/2)*(ln(J))**2)
+        # VelocityCont = inner(du,v)
+        FExtCont = inner(dv,Constant((0.00,0.0,0.0)))
+        FExtCont += inner(dv,-0.000001*v)
+        
+        # Psi += PsiCont
+        # Mass += MassCont
+        # Velocity += VelocityCont
+        # FExt += FExtCont
+        # construct = lambda f,fcont:
+        Psi = PsiCont if Psi is None else Psi + PsiCont
+        Mass = MassCont if Mass is None else Mass + MassCont
+        FExt = FExtCont if FExt is None else FExt + FExtCont
+        # Velocity = VelocityCont if Velocity is None else Velocity + VelocityCont
+
+    ThermalMass = inner(dT,rho*dTdt)*dx
+    xr = X0 + r
+    dist = sqrt(dot(jump(xr),jump(xr)))
+    overlap = (Constant(0.05)-dist)
+    ContactForm = -dot(jump(dvr),
+                      conditional(ge(overlap,0.0),-200.0*overlap,0.0)*jump(xr)/dist)*dc(0, metadata={"num_cells": 2,"special":"contact"})
+
     
+    Fform = -derivative(Psi,wx,dw)*dx + FExt*dx + ContactForm #- Mass*dx
+    Mform = Mass*dx #derivative(F,dwdt,Deldwdt)
+    AXform = derivative(Fform,wx,Delw)
+    AVform = derivative(Fform,wv,Delw)
+    
+    return Form(Fform),Form(Mform),Form(AXform),Form(AVform)
