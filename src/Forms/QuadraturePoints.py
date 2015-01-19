b1b7b9853fc05cf2dcad7ea5795731235a37dfac
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-19 15:58:09 -0800

    File for creating quadrature points.

---
+++ src/Forms/QuadraturePoints.py
@@ -0,0 +1,31 @@
+"""
+
+This file contains rules for cross section points.
+
+"""
+import numpy as np
+
+GPS1D = {
+    2:  [ [ np.sqrt(1.0/3.0), 1.0],
+              [-np.sqrt(1.0/3.0), 1.0] ],
+    3:  [ [ 0.0,       8.0/9.0 ],
+               [ np.sqrt(3.0/5.0), 5.0/9.0 ],
+               [-np.sqrt(3.0/5.0), 5.0/9.0 ] ]
+    }
+
+def RectOuterProd(ordx,ordy=None):
+    " Create an outer product rule for rectangles. "
+    if ordy==None:
+        ordy=ordx
+    GPS2D = []
+    for z1,w1 in GPS1D[ordx]:
+        for z2,w2 in GPS1D[ordy]:
+            GPS2D.append([z1,z2,w1*w2])
+    return GPS2D
+
+
+
+if __name__=="__main__":
+    print RectOuterProd(2,2)
+    print RectOuterProd(2,3)
+    print RectOuterProd(3)
