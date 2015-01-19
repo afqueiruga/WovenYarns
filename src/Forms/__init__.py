0ab52e4dfb0f5ae95bda5ffbb502683a168c7b16
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-18 17:42:21 -0800

    Created a subfolder for the forms.

---
+++ src/Forms/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+The UFL code for of the different forms that the fibrils can have.
+
+"""
+from MultiphysicsForm import MultiphysicsForm
+from IterativeForm import IterativeForm
+from BeamForm import BeamForm
+from ThermalForm import ThermalForm
