14c9bfbe550350e837dea829e04e499cf82a4a09
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-20 00:22:23 -0800

    Changed nomenclature, passes in t.

--- src/DIRK.py
+++ src/DIRK.py
@@ -17,8 +17,9 @@ class DIRK_Monolithic():
         self.V0 = MultiMeshFunction(warp.mmfs)
         self.DelW = MultiMeshFunction(warp.mmfs)
         if order==1:
-            self.RK_c = np.array([ 1.0 ])
+            self.RK_b = np.array([ 1.0 ], dtype.np.double)
             self.RK_a = np.array([ [1.0]], dtype=np.double)
+            self.RK_c = np.array([ 1.0 ], dtype.np.double)
         elif order==3:
             alpha = 0.43586652150845899942
             tau = (1.0+alpha)/2.0
@@ -27,22 +28,24 @@ class DIRK_Monolithic():
             self.RK_a = np.array([ [ alpha, 0.0, 0.0 ],
                                    [ tau-alpha, alpha, 0.0 ],
                                    [ b1, b2, alpha ] ], dtype=np.double)
-            self.RK_c = np.array( [ b1, b2, alpha ], dtype=np.double)
+            self.RK_b = np.array( [ b1, b2, alpha ], dtype=np.double)
+            self.RK_c = np.array( [ alpha, tau, 1.0 ], dtype=np.double)
         else:
             RK_alpha = 1.0-0.5*sqrt(2.0)
-            self.RK_c = np.array([ RK_alpha, 1.0 ])
+            self.RK_b = np.array([ RK_alpha, 1.0 ])
             self.RK_a = np.array([ [RK_alpha, 0.0],
                   [1.0-RK_alpha, RK_alpha] ], dtype=np.double)
+            self.RK_c = np.array([ RK_alpha, 1.0 ])
+
         
-        
-    def march(self):
+    def march(self,time=0.0):
         self.V0.vector()[:] = self.warp.wv.vector()[:]
         self.X0.vector()[:] = self.warp.wx.vector()[:]
         h = self.h
         KS = []
         VS = []
-        for i in xrange(len(self.RK_c)):
-            print " Stage ",i," at ",self.RK_c[i]," with aii=",self.RK_a[i,i]
+        for i in xrange(len(self.RK_b)):
+            print " Stage ",i," at ",self.RK_b[i]," with aii=",self.RK_a[i,i]
             eps = 1.0
             tol = 1.0e-10
             maxiter = 10
@@ -59,7 +62,7 @@ class DIRK_Monolithic():
 
                 K = self.warp.M - h*h*self.RK_a[i,i]**2*self.warp.AX - h*self.RK_a[i,i]*self.warp.AV
                 R = Rhat - self.warp.M*self.warp.wv.vector() + h*float(self.RK_a[i,i])*self.warp.R
-                self.bcapp(K,R,itcnt!=0)
+                self.bcapp(K,R,time+h*self.RK_c[i],itcnt!=0)
 
                 print "  Solving..."
                 solve(K,self.DelW.vector(),R)
