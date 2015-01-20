ba19c30745ab1035b1e2b0e05224b62e6d02a523
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-19 16:52:39 -0800

    Added reference, first rule in polar coordinates.

--- src/Forms/QuadraturePoints.py
+++ src/Forms/QuadraturePoints.py
@@ -2,7 +2,13 @@
 
 This file contains rules for cross section points.
 
+The 1D and thus outer product rules are commonly known Gauss-Legendre points.
+The circle points are the taken from those listed by Pavel Holoborodko on his page
+
+http://www.holoborodko.com/pavel/numerical-methods/numerical-integration/cubature-formulas-for-the-unit-disk/
+
 """
+
 import numpy as np
 
 GPS1D = {
@@ -23,7 +29,11 @@ def RectOuterProd(ordx,ordy=None):
             GPS2D.append([z1,z2,w1*w2])
     return GPS2D
 
+CircAngular2D = {4:[],8:[],16:[]}
+for i in xrange(4):
+    CircAngular2D[4].append([np.sqrt(2.0)/2.0,2.0*np.pi/4.0*(i), np.pi/4.0])
 
+CircCart2D = {}
 
 if __name__=="__main__":
     from matplotlib import pylab as plt
@@ -32,7 +42,9 @@ if __name__=="__main__":
         plt.ylim(-1,1)
         plt.plot([ z1 for z1,z2,w in pts], [z2 for z1,z2,w in pts],'x')
     plot_points(RectOuterProd(2,3))
+    plt.figure()
+    plot_points(RectOuterProd(2))
     plt.show()
-    print RectOuterProd(2,2)
-    print RectOuterProd(2,3)
-    print RectOuterProd(3)
+    print CircAngular2D
+
+    
