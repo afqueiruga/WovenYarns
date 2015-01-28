17d4dc8325b87925a0cba411764278cd44a627da
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-01-27 23:21:13 -0800

    Debugged.

--- src/Forms/ProblemDescription.py
+++ src/Forms/ProblemDescription.py
@@ -1,7 +1,5 @@
 from dolfin import *
 
-
-
 class ProblemDescription():
     """
     This is a prototype class representing a problem as
@@ -15,22 +13,27 @@ class ProblemDescription():
         be turned into a space of type Real. 
         """
         self.mesh = mesh
+        self.spaces = self.Declare_Spaces()
 
-        self.R = FunctionSpace(mesh,"Real")
         self.properties = {}
         for name,val in properties.iteritems():
             if val is Function:
                 self.properties[name] = val
             else:
-                self.properties[name] = Function(R)
+                self.properties[name] = Function(self.spaces['R'])
                 self.properties[name].interpolate(Constant(val))
 
-        self.spaces = self.Declare_Spaces()
+        self.fields = self.Declare_Fields()
         self.forms  = self.Build_Forms()
 
-    def Declare_Spaces():
+    def Declare_Spaces(self):
+        " Tabulate all function spaces used. By default need the space of real "
+        return {"R" : FunctionSpace(self.mesh,"Real",0)}
+    
+    def Declare_Fields(self):
+        " Tabulate all of the functions that are to be solved. "
         return {}
     
-    def Build_Form():
+    def Build_Forms(self):
         " Build the variational problem "
-        return None
+        return {}
