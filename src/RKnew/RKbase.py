cf9bd7a20eb87f230055d9c56556ca96bffba644
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-06-12 12:51:40 -0700

    Slowly debugging.

--- src/RKnew/RKbase.py
+++ src/RKnew/RKbase.py
@@ -1,26 +1,48 @@
 import numpy as np
 
-class RK():
-    def __init__(self,h, tableau, sys,update,bcapp, x,v, M):
+class RK_field():
+    """
+    order:
+       0 : implicit
+       1 : 1st order
+       2 : 2nd order
+    """
+    def __init__(self, order, u,M,sys,bcapp, update):
+        self.order = order
+        self.u = u
+        self.M = M
+        self.sys = sys
+        self.bcapp = bcapp
+        self.update = update
+
+        self.u0 = [ s.copy() for s in self.u ]
+        # if order == 0:
+        self.DU = [ s.copy() for s in self.u ]
+        if order == 2:
+            self.uhat = [ s.copy() for s in self.u ]
+    def save_u0(self):
+        for s,v in zip(self.u0,self.u):
+            s[:] = v[:]
+
+class RKbase():
+    def __init__(self,h, tableau, fields):
         self.h = h
         
-        self.sys = sys
-        self.update = update
-        self.bcapp = bcapp
-
         self.RK_a = tableau['a']
         self.RK_b = tableau['b']
         self.RK_c = tableau['c']
 
-        self.x = x
-        self.v = v
-
-        self.X0 = x.copy()
-        self.V0 = v.copy()
-        self.Xhat = x.copy()
-        self.DX = x.copy()
-
-        self.M = M
+        self.im_fields = []
+        self.ex_fields = []
+        for f in fields:
+            if f.order==0:
+                self.im_fields.append(f)
+            else:
+                self.ex_fields.append(f)
+    def DPRINT(*args):
+        for a in args[1:]:
+            print a,
+        print ""
         
     def march(self,time=0.0):
         pass
--- src/RKnew/exRK.py
+++ src/RKnew/exRK.py
@@ -1,6 +1,8 @@
 import numpy as np
 from dolfin import *
 
+from RKbase import *
+
 from IPython import embed
 
 """
@@ -40,50 +42,12 @@ exRK_table = {
         'c': np.array( [ 0.0, 0.5, 0.5, 1.0 ], dtype=np.double)
         }
     }
-
-
-
-class RK_field():
-    """
-    order:
-       0 : implicit
-       1 : 1st order
-       2 : 2nd order
-    """
-    def __init__(self, order, u,M,sys,bcapp, update):
-        self.order = order
-        self.u = u
-        self.M = M
-        self.sys = sys
-        self.bcapp = bcapp
-        self.update = update
-
-        self.u0 = [ s.copy() for s in self.u ]
-        # if order == 0:
-        self.DU = [ s.copy() for s in self.u ]
-        if order == 2:
-            self.uhat = [ s.copy() for s in self.u ]
-    def save_u0(self):
-        for s,v in zip(self.u0,self.u):
-            s[:] = v[:]
             
-class exRK():
-    def __init__(self,h, tableau, fields):
-        self.h = h
-        
-        self.RK_a = tableau['a']
-        self.RK_b = tableau['b']
-        self.RK_c = tableau['c']
-
-        self.im_fields = []
-        self.ex_fields = []
-        for f in fields:
-            if f.order==0:
-                self.im_fields.append(f)
-            else:
-                self.ex_fields.append(f)
-    def DPRINT(*args):
-        pass
+class exRK(RKbase):
+    """
+    Explicit Runge-Kuttas
+    Works on up to SemiExplicit Index-1 DAEs.
+    """
     def march(self,time=0.0):
         h = self.h
         RK_a = self.RK_a
@@ -125,7 +89,7 @@ class exRK():
                 while eps>tol and itcnt < maxiter:
                     self.DPRINT("  Solving...")
                     F,K = f.sys(time)
-                    f.bcapp(F,K,time+h*RK_c[i],itcnt!=0)
+                    f.bcapp(K,F, time+h*RK_c[i],itcnt!=0)
                     self.DPRINT( "   Solving Matrix... ")
                     if K is Matrix:
                         solve(K,f.DU[0],F)
@@ -177,7 +141,7 @@ class exRK():
             while eps>tol and itcnt < maxiter:
                 self.DPRINT("  Solving...")
                 F,K = f.sys(time)
-                f.bcapp(F,K,time+h*RK_c[i],itcnt!=0)
+                f.bcapp(K,F,time+h*RK_c[i],itcnt!=0)
                 self.DPRINT( "   Solving Matrix... ")
                 if K is Matrix:
                     solve(K,f.DU[0],F)
